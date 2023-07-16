2d6394eb948acca8b752fc7f4fc050fc50b9a4c4
DELL <DELL@DESKTOP-TU6C5D5>
2023-07-16 11:50:59 +0530

    Add project files.

---
+++ Assignment_Destop/CollectionWindow.cs
@@ -0,0 +1,103 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Assignment_Destop
+{
+    public partial class CollectionWindow : ObservableObject
+    {
+
+
+        [ObservableProperty]
+        public ObservableCollection<Student> students;
+        [ObservableProperty]
+        public Student? edit_student = null;
+
+
+
+
+        [RelayCommand]
+        public void RemoveStudent()
+        {
+            if (edit_student != null)
+            {
+                string name = edit_student.FirstName;
+                students.Remove(edit_student);
+                MessageBox.Show($"{name} is Deleted successfuly");
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select Student");
+
+
+            }
+        }
+
+        [RelayCommand]
+        public void AddStudent()
+        {
+            /*
+            var student = new Student(21, "wall", "King", "2001/08/04", 3.9, "4.png");
+            Person.Add(student);
+            */
+            var newWindow = new NewWindowVM();
+
+            newWindow.title = "ADD STUDENT";
+            NewWindow window = new NewWindow(newWindow);
+            window.ShowDialog();
+
+            if (newWindow.addStudent.FirstName != null)
+            {
+                students.Add(newWindow.addStudent);
+            }
+        }
+
+        [RelayCommand]
+        public void EditStudent()
+        {
+            if (edit_student != null)
+            {
+                var newWindow = new NewWindowVM(edit_student);
+                newWindow.title = "EDIT STUDENT";
+                var window = new NewWindow(newWindow);
+
+                window.ShowDialog();
+
+
+                int index = students.IndexOf(edit_student);
+                students.RemoveAt(index);
+                students.Insert(index, newWindow.addStudent);
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select the student");
+            }
+        }
+
+        public CollectionWindow()
+        {
+            students = new ObservableCollection<Student>();
+            students.Add(new Student(22, "Harry","Potter", "2000/12/01", 3.5, "1.png"));
+            students.Add(new Student(23, "Henry", "Petter", "1999/02/11", 3.2, "10.png"));
+            students.Add(new Student(22, "Charry", "Jhon", "2000/11/21", 3.7, "3.png"));
+            students.Add(new Student(21, "Paul",  "Lewi", "2001/03/03", 3.4, "7.png"));
+            students.Add(new Student(23, "Lewis",  "Sam", "1999/10/08", 3.5, "8.png"));
+            students.Add(new Student(22, "Rahul",  "Doll", "2000/04/01", 3.5, "2.png"));
+        }
+
+
+
+    }
+}
---
+++ Assignment_Destop/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_Destop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new CollectionWindow();
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void Listview_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void ShutDown_Click(object sender, EventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+    }
+}
